ce4f9b8f1bd3a1c859fc2fd4048ba4c11680b29a
NikitaGlock <nikita.glok0701@gmail.com>
2025-06-24 21:27:03 +0500

    1

--- js/game.js
+++ js/game.js
@@ -1,1 +1,27 @@
-// Главный файл игры
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#1d1d1d',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+let player;
+
+function preload() {
+    this.load.setBaseURL('.');
+    this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+}
+
+function create() {
+    player = this.add.image(400, 300, 'player');
+}
+
+function update() {}
+
+const game = new Phaser.Game(config);
