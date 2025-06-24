5bdede3a02ca75da82e4d4ea47f4de387e9f94fd
NikitaGlock <nikita.glok0701@gmail.com>
2025-06-24 21:47:01 +0500

    12

--- js/game.js
+++ js/game.js
@@ -3,7 +3,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#1d1d1d',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 800 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -12,16 +18,39 @@ const config = {
 };
 
 let player;
+let cursors;
 
 function preload() {
-    this.load.setBaseURL('.');
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/sky4.png');
+    this.load.image('ground', 'https://labs.phaser.io/assets/platform.png');
     this.load.image('player', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
 }
 
 function create() {
-    player = this.add.image(400, 300, 'player');
+    this.add.image(400, 300, 'sky');
+
+    const ground = this.physics.add.staticGroup();
+    ground.create(400, 580, 'ground').setScale(2).refreshBody();
+
+    player = this.physics.add.sprite(100, 450, 'player');
+    player.setCollideWorldBounds(true);
+    this.physics.add.collider(player, ground);
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-400);
+    }
+}
 
 const game = new Phaser.Game(config);
